2ec14721621c4f652e449136a7cc24ee5c260ca2
lxsca00 <ljimena10@gmail.com>
2023-01-17 14:31:58 -0500

    keys.js

---
+++ src/data/keys.js
@@ -0,0 +1,10 @@
+const keys = {
+    "endpoint": "https://api.micuentaweb.pe",
+    "username" : "62114337",
+    "password": "testpassword_t8DY7EZT5oF6IeGEljncoA00zSRQxu4gr1VH7NgNhsFMA",
+    "publickey" : "62114337:testpublickey_UbudXuMMngDU6ZbA7zkL1K7iV8kFnxQYjtyKc3j8tRcJj",
+    "HMACSHA256" : "vg8VSiWelsfgFO8zwzTfuqM3u5Kfh1FjyUvBdllWdKBHw"
+}
+
+
+module.exports =  keys
